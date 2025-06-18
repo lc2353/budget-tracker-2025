031320094cc62ff4a87d7ac93013e78b50d30a5d
lc2353 <120254125+lc2353@users.noreply.github.com>
2025-06-19 00:23:55 +0100

    update transaction model

--- backend/internal/models/transaction.go
+++ backend/internal/models/transaction.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Transaction struct {
-	ID                  string    `json:"id,omitempty" firestore:"-"`
+	ID                  string    `json:"id" firestore:"-"`
 	UserID              string    `json:"userId" firestore:"userId"`
 	TransactionDateTime time.Time `json:"transactionDateTime" firestore:"transactionDateTime"`
-	Description         string    `json:"description" firestore:"description"`
-	Amount              int32     `json:"amount" firestore:"amount"` // will be stored as pence
-	Category            string    `json:"category" firestore:"category"`
-	Type                string    `json:"type" firestore:"type"` // "income", "expense", "transfer"
+	Description         string    `json:"description,omitempty" firestore:"description"`
+	Amount              int32     `json:"amount" firestore:"amount"`
+	Category            string    `json:"category,omitempty" firestore:"category"`
+	Type                string    `json:"type" firestore:"type"`
 	BankReference       string    `json:"bankReference,omitempty" firestore:"bankReference,omitempty"`
 	InsertedAt          time.Time `json:"insertedAt" firestore:"insertedAt"`
 	UpdatedAt           time.Time `json:"updatedAt" firestore:"updatedAt"`
